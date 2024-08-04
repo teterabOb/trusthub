231f26e7d62027a01e1703c9a9ade32d1eda5126
Franco Carballar <francocarballar@gmail.com>
2024-08-04 13:45:33 -0300

    feat: :lipstick: actualiza la página de incio

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { DebugContracts } from "./debug/_components/DebugContracts";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { Address } from "~~/components/scaffold-eth";
@@ -12,12 +13,13 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-4xl font-bold">Mi reputación</span>
+            <span className="block text-4xl font-bold">Verificar mediante el protocolo</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-normal">Billetera conectada:</p>
             <Address address={connectedAddress} />
           </div>
+          <DebugContracts />
         </div>
       </div>
     </>
